4b50eba6451c9742395406d5973b97d70da0012f
Eider <45559339+eidersuso@users.noreply.github.com>
2022-06-06 20:20:13 +0200

    Add files via upload

---
+++ minimainimou/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+  
+  
+    $('#imagen-entera').fadeIn(5500).css('display', 'block');
+    $('.underConstruct').fadeIn(5500);
+    
+     
+     
+     
+   });//fin document
